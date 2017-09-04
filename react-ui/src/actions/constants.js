db653b319e0e5e639f33618f0661e820faaee563
Dustin D'Avignon <ddavignon@csumb.edu>
2017-09-03 19:38:07 -0700

    add slight button spacing

--- react-ui/src/actions/constants.js
+++ react-ui/src/actions/constants.js
@@ -1,4 +1,4 @@
-export const ROOT_URL = ''; // 'http://localhost:5001';
+export const ROOT_URL = ''; // http://localhost:5001';
 export const AUTH_HEADERS = { 'Authorization': 'whatever-you-want', 'Accept': 'application/json', };
 export function guid() {
     function s4() {
